3df3a07a486942e0c8a18e85b54659040087dd24
juanserdp <90122446+juanserdp@users.noreply.github.com>
2021-10-01 19:13:37 -0500

    ventas

---
+++ src/components/ventas/Modal.js
@@ -0,0 +1,252 @@
+import React, { Component, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button, Form } from 'react-bootstrap';
+import ProductosAgg from './ProductosAgg.js';
+import ListaProductos from '../productos/ListaProductos.js';
+
+
+
+export default class ModalWindow extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleAgregarProducto = this.handleAgregarProducto.bind(this);
+        this.handleEliminarProducto = this.handleEliminarProducto.bind(this);
+        this.handleValor = this.handleValor.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            show: false,
+            identificador: '',
+            valor: 0,
+            cantidad: 0,
+            productos: [{
+                Id_producto: '',
+                descripcion: '',
+                valor_unitario: 0,
+                estado: ''
+            }
+            ],
+            fecha: '',
+            identificacionCliente: '',
+            nombreCliente: '',
+            nombreVendedor: ''
+        }
+    }
+
+    handleChange(evento) {
+        const nombre = evento.target.name;
+        const valor = evento.target.value;
+        this.setState({
+            [nombre]: valor,
+        });
+    }
+
+    handleClose() {
+        this.setState({
+            show: false
+        });
+    }
+
+    handleShow() {
+        this.setState({
+            show: true
+        });
+    }
+
+    handleValor(productos) {
+        var total = 0;
+        for (var i = 0; i < productos.length; i++) {
+            total += productos[i].valor_unitario
+        }
+        this.setState({
+            valor: total
+        });
+    }
+
+    handleAgregarProducto(productoNuevo) {
+        this.setState({
+            productos: [...this.state.productos, productoNuevo],
+            cantidad: this.state.cantidad + 1
+        });
+    }
+
+    handleEliminarProducto(productoId) {
+        const productos = this.state.productos;
+        this.setState({
+            productos: productos.filter(
+                (producto, i) => {
+                    return i !== productoId
+                }
+            ),
+            cantidad: this.state.cantidad - 1
+        });
+    }
+
+    handleSubmit(){
+        this.props.handleAgregarVenta(this.state)
+        this.setState({
+            show: false,
+            identificador: '',
+            valor: 0,
+            cantidad: 0,
+            productos: [{
+                Id_producto: '',
+                descripcion: '',
+                valor_unitario: 0,
+                estado: ''
+            }
+            ],
+            fecha: '',
+            identificacionCliente: '',
+            nombreCliente: '',
+            nombreVendedor: ''
+        });
+    }
+
+    render() {
+        //const [show, setShow] = useState(false);
+        const identificador = this.state.identificador;
+        const valor = this.state.valor;
+        const cantidad = this.state.cantidad;
+        const fecha = this.state.fecha;
+        const identificacionCliente = this.state.identificacionCliente;
+        const nombreCliente = this.state.nombreCliente;
+        const nombreVendedor = this.state.nombreVendedor;
+        return (
+            <>
+                <Button variant="primary" onClick={this.handleShow}>
+                    + Nueva venta
+                </Button>
+                <Modal fullscreen={true} show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Ingresar Nueva Venta</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Id venta:</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    name="identificador"
+                                    id="identificador"
+                                    placeholder="id"
+                                    value={identificador}
+                                    onChange={this.handleChange} />
+                            </Form.Group>
+
+                            <p />
+                            <Form.Group>
+                                <label for="recipient-name" class="col-form-label">Responsable: </label>
+                                <select name="editarresponsable" id="responsable">
+                                    <option value="">Selecciona una opción</option>
+                                    <option value="">Jennifer Monroy</option>
+                                    <option value="">Yuliana Gaviria</option>
+                                    <option value="">Jhon Edinson Cruz</option>
+                                    <option value="">Juan Sebastian Rodriguez</option>
+                                    <option value="">Julio Cesar Gutierrez</option>
+                                </select>
+                            </Form.Group>
+                            <p />
+
+                            <Form.Group>
+                                <Form.Label>Fecha:</Form.Label>
+                                <Form.Control
+                                    type="date"
+                                    name="fecha"
+                                    id="fecha"
+                                    value={fecha}
+                                    onChange={this.handleChange}
+                                />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Nombre del Cliente:</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="nombreCliente"
+                                    id="nombreCliente"
+                                    placeholder="Escriba el nombre del cliente..."
+                                    value={nombreCliente}
+                                    onChange={this.handleChange} />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label># indentificacion:</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    name="identificacionCliente"
+                                    id="identificacionCliente"
+                                    placeholder="Escriba el numero de idenficacion del cliente..."
+                                    value={identificacionCliente}
+                                    onChange={this.handleChange} />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Cantidad:</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    name="cantidad"
+                                    id="cantidad"
+                                    readOnly
+                                    value={cantidad}
+                                    onChange={this.handleChange} />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <ListaProductos
+                                    productos={this.state.productos}
+                                    estoyEnVentas={true}
+                                    handleAgregarProducto={this.handleAgregarProducto}
+                                    handleEliminarProducto={this.handleEliminarProducto} />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <ProductosAgg
+                                    productos={this.state.productos}
+                                    estoyEnVentas={true}
+                                    handleAgregarProducto={this.handleAgregarProducto}
+                                    handleEliminarProducto={this.handleEliminarProducto} />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Valor Total:</Form.Label>
+                                <Form.Control 
+                                type="number" 
+                                name="valor"
+                                readOnly 
+                                value={valor} />
+                            </Form.Group>
+                            <p />
+
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <label for="recipient-name" class="col-form-label">Estado de la venta: </label>
+                                <select name="editarEstadoVenta" id="editarEstadoVenta">
+                                    <option value="">Selecciona una opción</option>
+                                    <option value="">Creación</option>
+                                    <option value="">Embalaje</option>
+                                    <option value="">Despacho</option>
+                                    <option value="">Recepción</option>
+                                    <option value="">Ubicación</option>
+                                    <option value="">Completada</option>
+                                </select>
+                            </Form.Group>
+
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Cerrar
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose} onClick={this.handleSubmit}>
+                            Guardar cambios
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
+}
+
+
--- src/components/ventas/Ventas.js
+++ src/components/ventas/Ventas.js
@@ -1,26 +1,133 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react'
 
 import Navigation from '../globals/Navigation'
+import ModalWindow from './Modal.js'
+import {
+    Button
+} from "reactstrap";
 
 
+function Venta(props){
+    const ventas = props.ventas.map(
+        (venta, indice) => {
+            return(
+                <tr key={indice}>
+                    <td>{venta.identificador}</td>
+                    <td>{venta.valor}</td>
+                    <td>{venta.cantidad}</td>
+                    <td>{venta.precio}</td>
+                    <td>{venta.fecha}</td>
+                    <td>{venta.indentificacionCliente}</td>
+                    <td>{venta.nombreCliente}</td>
+                    <td>{venta.nombreVendedor}</td>
+                    <td><Button color="primary" onClick={() => props.editarVenta(indice) } onClick={props.handleShow}  >Editar</Button>
+                        {" "}
+                        <Button color="danger" onClick={() => props.eliminarVenta(indice)} >Eliminar</Button>
+                    </td>
+                </tr>
+            )
+        }
+    )
+    return ventas
+}
 
+const data = [
+    { Id_producto: 1001, descripcion: "Taladro percutor 3 pulgadas", valor_unitario: 120000, estado: "Disponible" },
+    { Id_producto: 1002, descripcion: "Almadana 3 lb Mango fibra de Vidrio", valor_unitario: 250000, estado: "No_Disponible" },
+];
 
 export default class Inicio extends Component {
+    constructor(props) {
+        super(props);
+        this.handleAgregarVenta = this.handleAgregarVenta.bind(this);
+        this.eliminarVenta = this.eliminarVenta.bind(this);
+        this.editarVenta = this.editarVenta.bind(this);
+        this.state = {
+            ventas: [
+                {
+                    identificador: '1',
+                    valor: '370000',
+                    cantidad: '2',
+                    productos: data,
+                    fecha: '2021-11-02',
+                    identificacionCliente: '10974635142',
+                    nombreCliente: 'cliente1',
+                    nombreVendedor: 'vendedor1'
+                },
+            ],
+        }
+    };
+
+    //agregarVenta = () => { };
+
+    handleAgregarVenta(nuevaVenta) {
+        //const ventas = this.state.ventas;
+        this.setState({
+            ventas: [...this.state.ventas, nuevaVenta]
+        });
+    }
+
+    //Eliminar una venta
+
+    eliminarVenta(identificador) {
+        const ventas = this.state.ventas;
+        this.setState({
+            ventas: ventas.filter( (venta, indiceMatriz) => {
+                return indiceMatriz !== identificador
+            })
+        });
+    }
+
+    editarVenta() {
+
+    }
+    handleShow() {
+        render() 
+        {render (<ModalWindow />)}
+    }
+
+    
+
     render() {
+        const ventas = this.state.ventas;
+
         return (
-            
             <div className="contenedor-app">
                 <Navigation />
-            <div className="contenedor mt-3">
-                <main>
-                    <h1>Ventas</h1>
-                    <p>Esta es la lista de ventas</p>
-                </main>
+                <div className="contenedor mt-3">
+                    <main>
+                        <h1>Ventas</h1>
+                        <p>Esta es la lista de ventas</p>
+                    </main>
+                    <p />
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Valor Total</th>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Fecha</th>
+                                <th scope="col"># indentificacion</th>
+                                <th scope="col">Nombre Cliente</th>
+                                <th scope="col">Nombre del Vendedor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <Venta  
+                            handleShow={this.handleShow} 
+                            ventas={ventas} 
+                            eliminarVenta={this.eliminarVenta} 
+                            editarVenta={this.editarVenta}  />
+                        </tbody>
+                    </table>
+                    <ModalWindow 
+                    datosVentas={this.state} 
+                    handleAgregarVenta={this.handleAgregarVenta}
+                    ventas={this.state.ventas}  />
+                </div>
             </div>
-        </div>
-
         );
-           
-        
     }
 }
