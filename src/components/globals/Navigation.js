425a0653d7be106144cc69dd60d788dc4bdc76b3
Juliocg2021 <88988826+Juliocg2021@users.noreply.github.com>
2021-10-05 05:15:04 -0500

    Update Navigation.js

--- src/components/globals/Navigation.js
+++ src/components/globals/Navigation.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
